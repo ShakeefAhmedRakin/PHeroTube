b19146f24cbb346eb97bad2100ca0375e9533df2
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-09-01 06:46:24 +0600

    Completed the video section and made it fully dynamic and responsive.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const displayCategoryButton = async (categoryData) => {
   updateVideoSection(1000);
 };
 
-// DISPLAY VIDEOS BASED ON CATEGORY ID FUNCTION
+// UPDATE VIDEOS BASED ON CATEGORY ID FUNCTION
 const updateVideoSection = async (category_id) => {
   // UPDATING BUTTON COLOR
   categoryContainer.childNodes.forEach((button) => {
@@ -46,6 +46,115 @@ const updateVideoSection = async (category_id) => {
       button.classList.remove("text-white");
     }
   });
+  storeVideoInCache(category_id);
+};
+
+// DISPLAY VIDEOS FUNCTION
+const videoContainer = document.getElementById("video-container");
+const noVideoElement = document.getElementById("no-video-element");
+let videoCache = [];
+const storeVideoInCache = async (category_id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${category_id}`
+  );
+  const allVideoData = await response.json();
+
+  videoContainer.innerHTML = "";
+  videoContainer.classList.remove("hidden");
+  videoContainer.classList.add("flex");
+  videoCache = [];
+  if (allVideoData.status) {
+    // REMOVING THE 'NO VIDEO FOUND' ELEMENT IF IT EXISTS
+    noVideoElement.classList.remove("flex");
+    noVideoElement.classList.add("hidden");
+
+    // LOOPING THROUGH EACH VIDEO DATA
+    allVideoData.data.forEach((videoData) => {
+      videoCache.push(videoData);
+      displayVideo(videoData);
+    });
+
+    // SHOWING VIDEOS
+  } else {
+    // DISPLAYING THE 'NO VIDEO ELEMENT' ELEMENT
+    noVideoElement.classList.add("flex");
+    noVideoElement.classList.remove("hidden");
+    // HIDING VIDEO CONTAINER
+    videoContainer.classList.add("hidden");
+    videoContainer.classList.remove("flex");
+  }
+  console.log(videoCache);
+};
+
+// DISPLAY A SINGLE VIDEO DATA.
+const displayVideo = (videoData) => {
+  console.log(videoData.others.posted_date);
+  videoContainer.innerHTML += `<a href="#">
+        <div class="w-full space-y-5">
+          <!-- THUMBNAIL -->
+          <div class="w-full relative">
+            <!-- IMAGE -->
+            <img
+              src="${videoData?.thumbnail}"
+              alt="thumb"
+              class="rounded-xl w-full h-48 object-cover"
+            />
+            <!-- TAG -->
+            <span
+              class="bg-[#171717] text-white text-[10px] p-1 rounded-md absolute right-1 bottom-1"
+              >${
+                videoData?.others?.posted_date
+                  ? secondsToTime(videoData?.others?.posted_date)
+                  : ""
+              }</span
+            >
+          </div>
+          <!-- DETAILS CONTAINER -->
+          <div class="flex gap-3">
+            <!-- PROFILE PICTURE -->
+            <img
+              src="${videoData?.authors[0]?.profile_picture}"
+              alt="profile"
+              class="w-10 h-10 rounded-full object-cover"
+            />
+            <!-- DETAILS -->
+            <div
+              class="flex flex-col items-start justify-center gap-y-1"
+            >
+              <!-- TITLE -->
+              <h1 class="font-bold">
+                ${videoData?.title}
+              </h1>
+              <!-- CREATOR AND VERIFIED -->
+              <div class="flex gap-x-[9px]">
+                <p class="text-sm text-[#5d5d5d]">${
+                  videoData?.authors[0]?.profile_name
+                }</p>
+                <div class="flex justify-center items-center">
+                <img src="${
+                  videoData?.authors[0]?.verified
+                    ? "icons8-verified-50.png"
+                    : ""
+                }" class="w-4 h-4" />
+                </div>
+              </div>
+              <!-- VIEWS -->
+              <p class="text-sm text-[#5d5d5d] mt-[3px]">${
+                videoData?.others?.views
+              } views</p>
+            </div>
+          </div>
+        </div>
+      </a>`;
+};
+
+// SECOND TO TIME FORMAT FUNCTION
+
+const secondsToTime = (seconds) => {
+  seconds = parseInt(seconds);
+  let hours = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds % 3600) / 60);
+  return `${hours} hrs ${minutes} min ago`;
 };
 
 fetchCategory();
